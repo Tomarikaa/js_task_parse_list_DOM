02f46a55368217659a33f4c2676860a8b1534d02
Tamara Beruashvili <tomaberuashvili@gmail.com>
2024-11-17 21:40:38 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+function parseSalary(salary) {
+  return parseInt(salary.replace('$', '').replace(',', ''));
+}
+
+function sortList(list) {
+  const items = Array.from(list.children);
+
+  items.sort((a, b) => {
+    const salaryA = parseSalary(a.getAttribute('data-salary'));
+    const salaryB = parseSalary(b.getAttribute('data-salary'));
+    return salaryB - salaryA;
+  });
+
+  // Re-append the sorted items back to the list
+  items.forEach(item => list.appendChild(item));
+}
+
+const employeeList = document.querySelector('ul');
+sortList(employeeList);

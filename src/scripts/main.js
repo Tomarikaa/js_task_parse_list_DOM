540b1e36108505f74c590e9be2b01af09c604a7e
Tamara Beruashvili <tomaberuashvili@gmail.com>
2024-11-18 09:55:58 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,12 +10,28 @@ function sortList(list) {
   items.sort((a, b) => {
     const salaryA = parseSalary(a.getAttribute('data-salary'));
     const salaryB = parseSalary(b.getAttribute('data-salary'));
+
     return salaryB - salaryA;
   });
 
   // Re-append the sorted items back to the list
-  items.forEach(item => list.appendChild(item));
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  const items = Array.from(list.children);
+
+  return items.map((item) => {
+    return {
+      name: item.textContent.trim(),
+      position: item.getAttribute('data-position'),
+      salary: parseSalary(item.getAttribute('data-salary')),
+      age: parseInt(item.getAttribute('data-age')),
+    };
+  });
 }
 
 const employeeList = document.querySelector('ul');
+
 sortList(employeeList);
+getEmployees(employeeList);
